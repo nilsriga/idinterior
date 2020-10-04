3065c8b76a99d5e77675408cb7591c9499a5f505
root <root@idinterior.lv>
2020-10-04 14:50:59 +0300

    updates

--- js/agency.js
+++ js/agency.js
@@ -9,7 +9,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 65
+            scrollTop: $($anchor.attr('href')).offset().top 
         }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
